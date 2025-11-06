12df5ab8ec28fb9eea3a288cb59a553185e2a1e2
Muhammed Ali CAN <canpacis@gmail.com>
2025-11-06 12:17:26 +0300

    feat: register hello-world handler

--- cmd/server.go
+++ cmd/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"log/slog"
 	"net/http"
@@ -9,10 +10,42 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/canpacis/go-assignment/internal/errs"
+	"github.com/canpacis/go-assignment/services/greet"
 )
 
 func main() {
 	mux := http.NewServeMux()
+
+	greetService := &greet.Service{}
+
+	// Inlining this http.Handler so as not to overengineer a solution.
+	// A helper function that can generate & validate the payload, run the
+	// service method and write any result/error to the client can replace this.
+	mux.HandleFunc("GET /hello-world", func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		query := r.URL.Query()
+
+		resp, err := greetService.Greet(ctx, &greet.GreetPayload{Name: query.Get("name")})
+		w.Header().Set("Content-Type", "application/json")
+
+		encoder := json.NewEncoder(w)
+
+		if err != nil {
+			var apiErr *errs.Error
+			if errors.As(err, &apiErr) {
+				w.WriteHeader(apiErr.Status)
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+
+			encoder.Encode(err)
+		} else {
+			w.WriteHeader(http.StatusOK)
+			encoder.Encode(resp)
+		}
+	})
 
 	Serve(mux)
 }
